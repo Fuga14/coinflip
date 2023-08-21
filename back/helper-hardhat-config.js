c48ce041cf821e54a668c73b21e25a9f9aa5f28d
Fuga14 <vladyslavpereder@gmail.com>
2023-08-21 23:49:42 +0300

    deployed to sepoila and mumbai tests net

--- back/helper-hardhat-config.js
+++ back/helper-hardhat-config.js
@@ -19,7 +19,7 @@ const networkConfig = {
   },
   80001: {
     name: 'mumbai',
-    subscriptionId: '5742',
+    subscriptionId: '5763',
     linkToken: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
     keyHash: '0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f',
     vrfCoordinator: '0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed',
