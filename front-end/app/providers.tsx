0c0061b43a962eaedad2b1195516fcf421dd3261
Fuga14 <vladyslavpereder@gmail.com>
2023-08-29 11:57:16 +0300

    added web3 providers and connect button

---
+++ front-end/app/providers.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import * as React from 'react';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { argentWallet, trustWallet, ledgerWallet } from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { sepolia, polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [sepolia, polygonMumbai],
+  [publicProvider()]
+);
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || '';
+
+const { wallets } = getDefaultWallets({
+  appName: 'CryptoFlip',
+  projectId,
+  chains,
+});
+
+const demoAppInfo = {
+  appName: 'CryptoFlip',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        appInfo={demoAppInfo}
+        modalSize="compact"
+        theme={darkTheme()}
+        showRecentTransactions={true}
+      >
+        {mounted && children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
